8a05fa90aa7b9f056c4d6d3c9a728ec19fa55b2d
Shen Sandaru Rathnayaka <136662438+ShenSandaru@users.noreply.github.com>
2024-06-28 10:25:13 +0530

    required files created

---
+++ test-03/back-end/src/controllers/userController.js
@@ -0,0 +1,26 @@
+const User = require('../models/User');
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// Implement other controller functions (getUserById, updateUser, deleteUser)
